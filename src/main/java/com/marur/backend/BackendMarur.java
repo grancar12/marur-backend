7e2030351d0be2de0bbaa7b9cef7b7fd053eaa64
grancar12 <loscamacho19@gmail.com>
2026-05-22 19:17:40 -0500

    correcion de errores en llaves

--- src/main/java/com/marur/backend/BackendMarur.java
+++ src/main/java/com/marur/backend/BackendMarur.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendApplication {
+public class BackendMarur {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+		SpringApplication.run(BackendMarur.class, args);
 	}
 
 }
+
+
